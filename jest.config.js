982a781b357f5b47228950181d36009610fa4ead
osmanyqo <osmany@qordev.com>
2023-02-05 13:54:17 -0600

    feat(project): I add custom paths to jest

--- jest.config.js
+++ jest.config.js
@@ -1,5 +1,12 @@
 /** @type {import('ts-jest').JestConfigWithTsJest} */
 module.exports = {
   preset: 'ts-jest',
-  testEnvironment: 'node'
+  testEnvironment: 'node',
+  moduleNameMapper: {
+    '@src/(.*)': '<rootDir>/src/$1',
+    '@core/(.*)': '<rootDir>/src/core/$1',
+    '@services/(.*)': '<rootDir>/src/services/$1',
+    '@api/(.*)': '<rootDir>/src/api/$1',
+    '@app/(.*)': '<rootDir>/src/app/$1'
+  }
 }
